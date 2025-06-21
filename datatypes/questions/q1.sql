6ecf3758e4757316bd934e86238a80bca8780791
MohanadGad38 <90938419+MohanadGad38@users.noreply.github.com>
2025-06-22 00:07:38 +0300

    add: q1

---
+++ datatypes/questions/q1.sql
@@ -0,0 +1,58 @@
+
+
+-- Try a hands-on exercise to test your knowledge of JSON data types.
+-- Problem statement
+
+-- You will perform the following tasks to check your understanding of JSON data type.
+-- Task 1
+
+-- Create a table cars with two columns and insert the following data:
+-- car_id 	car_info
+-- 1 	brand: ferrari, model: testarosa
+-- 2 	brand: aston martin, model: db2
+-- 3 	brand: bentley, model: mulsanne
+-- 4 	brand: ford, model: T
+
+-- Expected output
+
+-- When you’re done creating the table and inserting the data in the table, you may see the records in the cars table by using the following query.
+
+-- Select * from cars;
+
+-- It will give the following output.
+
+-- car_id  car_info
+-- 1       {"brand": "ferrari", "model": "testarosa"}
+-- 2       {"brand": "aston martin", "model": "db2"}
+-- 3       {"brand": "bentley", "model": "mulsanne"}
+-- 4       {"brand": "ford", "model": "T"}
+
+CREATE TABLE cars (
+    car_id SERIAL PRIMARY KEY,
+    car_info JSONB
+);
+
+INSERT INTO cars (car_info)
+VALUES ('{"brand": "ferrari", "model": "testarosa"}');
+
+INSERT INTO cars (car_info)
+VALUES ('{"brand": "aston martin", "model": "db2"}');
+
+INSERT INTO cars (car_info)
+VALUES ('{"brand": "bentley", "model": "mulsanne"}');
+
+INSERT INTO cars (car_info)
+VALUES ('{"brand": "ford", "model": "t"}');
+
+SELECT * FROM cars;
+
+
+-- Task 2
+
+-- Retrieve the record having the information on Ferrari from the cars table.
+-- Expected output
+
+-- The output for task two looks like the following:
+
+-- car_id  car_info
+-- 1       {"brand": "ferrari", "model": "testarosa"}
